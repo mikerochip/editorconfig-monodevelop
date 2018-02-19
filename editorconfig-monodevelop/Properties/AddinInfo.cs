60f30fea5ff114acbb25355ad64820b8472fc826
Michael Schweitzer <mschweitzer@gmail.com>
2018-02-18 17:45:12 -0800

    Cleaned up Addin description and added supported features to it.

--- editorconfig-monodevelop/Properties/AddinInfo.cs
+++ editorconfig-monodevelop/Properties/AddinInfo.cs
@@ -10,5 +10,16 @@ using Mono.Addins.Description;
 
 [assembly: AddinName("EditorConfig")]
 [assembly: AddinCategory("IDE extensions")]
-[assembly: AddinDescription("Adds support for EditorConfig http://editorconfig.org. Files will be modified on save.")]
 [assembly: AddinAuthor("Michael Schweitzer")]
+[assembly: AddinDescription(
+    "Adds support for EditorConfig http://editorconfig.org. "
+    + "Files are modified on save, and there is a new Edit menu command.\n"
+    + "\nSupported features:\n"
+    + "\nindent_style"
+    + "\nindent_size"
+    + "\ntab_width"
+    + "\nend_of_line"
+    + "\ncharset"
+    + "\ntrim_trailing_whitespace"
+    + "\ninsert_final_newline"
+)]
