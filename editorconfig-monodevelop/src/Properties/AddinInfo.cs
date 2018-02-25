3ab204947c749ae87626287a42e02b1a1cb05cf3
Michael Schweitzer <mschweitzer@gmail.com>
2018-02-25 14:37:09 -0800

    Elaborating on addin description and cleaned up Edit menu text.

--- editorconfig-monodevelop/src/Properties/AddinInfo.cs
+++ editorconfig-monodevelop/src/Properties/AddinInfo.cs
@@ -12,8 +12,7 @@ using Mono.Addins.Description;
 [assembly: AddinCategory("IDE extensions")]
 [assembly: AddinAuthor("Michael Schweitzer")]
 [assembly: AddinDescription(
-    "Adds support for EditorConfig http://editorconfig.org."
-    + "Implementation generally follows this post: https://github.com/editorconfig/editorconfig/issues/248#issuecomment-166980703"
+    "Adds support for EditorConfig http://editorconfig.org"
     + "\n"
     + "\nSupported features:\n"
     + "\nindent_style"
@@ -23,4 +22,16 @@ using Mono.Addins.Description;
     + "\ncharset"
     + "\ntrim_trailing_whitespace"
     + "\ninsert_final_newline"
+    + "\n"
+    + "\nImplementation generally follows this post: https://github.com/editorconfig/editorconfig/issues/248#issuecomment-166980703"
+    + "\nThese are file open features:"
+    + "\n indent_style"
+    + "\n indent_size"
+    + "\n tab_width"
+    + "\n end_of_line"
+    + "\nThese are file save features:"
+    + "\n charset"
+    + "\n trim_trailing_whitespace"
+    + "\n insert_final_newline"
+    + "\n end_of_line (enabled via an option)"
 )]
